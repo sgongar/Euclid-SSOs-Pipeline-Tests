93c4aab978684f5abb4fa8bf88865184af90ce7c
Samuel Gongora-Garcia <s.gongoragarcia@gmail.com>
2018-08-25 10:47:39 +0200

    test

--- scamp_catalog_elvis_stars.py
+++ scamp_catalog_elvis_stars.py
@@ -86,6 +86,7 @@ def create_catalog():
     scamp_df = create_scamp_df()
 
     test_function(scamp_df)
+    raise Exception
 
     unique_sources = stars_df['IDX']
     total_stars = stars_df['IDX'].size
