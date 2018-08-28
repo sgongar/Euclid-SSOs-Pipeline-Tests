9bb7c72ace0376443c7d84675d8202e2a5137285
Samuel Gongora-Garcia <s.gongoragarcia@gmail.com>
2018-08-28 21:14:03 +0200

    update

--- regressions/mag_auto_vs_b_image.R
+++ regressions/mag_auto_vs_b_image.R
@@ -1,28 +1,28 @@
 # Read CSV into R
-SSOsDataFrame <- read.csv(file="ssos_2.csv", header=TRUE, sep=",")
+SSOsDataFrame <- read.csv(file="ssos_2_upper_24_5.csv", header=TRUE, sep=",")
+StarsDataFrame <- read.csv(file="scamp_stars.csv", header=TRUE, sep=",")
 
 library(MASS)
 library(ggplot2)
 library(boot)
 
-# cv_MSE_k10 <- rep(NA,10)
-# 
-# for (i in 1:10) {
-#   modelo <- glm(B_IMAGE ~ poly(MAG_AUTO, i, raw=TRUE), data = SSOsDataFrame)
-#   set.seed(17)
-#   cv_MSE_k10[i] <- cv.glm(data = SSOsDataFrame, glmfit = modelo, K = 10)$delta[1]
-# }
-# p4 <- ggplot(data = data.frame(polinomio = 1:10, cv_MSE = cv_MSE_k10),
-#              aes(x = polinomio, y = cv_MSE)) +
-#       geom_point(colour = c("firebrick3")) +
-#       geom_path()
-# p4 <- p4 + theme(panel.grid.major = element_line(colour  =  'gray90'))
-# p4 <- p4 + theme(plot.title = element_text(face  =  'bold'))
-# p4 <- p4 + theme(panel.background = element_rect(fill  =  'gray98'))
-# p4 <- p4 + labs(title  =  'Test Error ~ Grado del polinomio')
-# p4 <- p4 + scale_x_continuous(breaks = 1:10)
-# print(p4)
+cv_MSE_k10 <- rep(NA,10)
 
+for (i in 1:10) {
+  modelo <- glm(B_IMAGE ~ poly(MAG_AUTO, i, raw=TRUE), data = SSOsDataFrame)
+  set.seed(17)
+  cv_MSE_k10[i] <- cv.glm(data = SSOsDataFrame, glmfit = modelo, K = 10)$delta[1]
+}
+p4 <- ggplot(data = data.frame(polinomio = 1:10, cv_MSE = cv_MSE_k10),
+             aes(x = polinomio, y = cv_MSE)) +
+      geom_point(colour = c("firebrick3")) +
+      geom_path()
+p4 <- p4 + theme(panel.grid.major = element_line(colour  =  'gray90'))
+p4 <- p4 + theme(plot.title = element_text(face  =  'bold'))
+p4 <- p4 + theme(panel.background = element_rect(fill  =  'gray98'))
+p4 <- p4 + labs(title  =  'Test Error ~ Grado del polinomio')
+p4 <- p4 + scale_x_continuous(breaks = 1:10)
+# print(p4)
 
 out <- ggplot(data = SSOsDataFrame, aes(x = MAG_AUTO, y = B_IMAGE)) +
               geom_point(color = "grey30", alpha = 0.3) + 
@@ -31,28 +31,37 @@ out <- ggplot(data = SSOsDataFrame, aes(x = MAG_AUTO, y = B_IMAGE)) +
               theme_bw() +
               theme(plot.title = element_text(hjust = 0.5))
 
-print(out)
+# print(out)
 
-modelo_poli5 <- lm(B_IMAGE ~ poly(MAG_AUTO, 5, raw=TRUE), data = SSOsDataFrame)
+# Ajuste hasta 24'5
+modelo_poli5 <- lm(B_IMAGE ~ poly(MAG_AUTO, 4, raw=TRUE), data = SSOsDataFrame)
 # print(summary(modelo_poli5))
 # print(modelo_poli5$coefficients)
 
-mpi <- cbind(SSOsDataFrame, predict(modelo_poli5, interval = "prediction"))
+prediction.level <- 0.9
+mpi <- cbind(SSOsDataFrame, predict(modelo_poli5, interval = "prediction", level = prediction.level))
 out_2 <- ggplot(mpi, aes(x = SSOsDataFrame$MAG_AUTO)) +
                 geom_ribbon(aes(ymin = lwr, ymax = upr), fill = "gray", alpha = 0.5) +
-                geom_point(aes(y = SSOsDataFrame$B_IMAGE)) +
+                geom_point(aes(y = SSOsDataFrame$B_IMAGE), color = "red") +
+                # points(x = StarsDataFrame$MAG_AUTO, y = StarsDataFrame$B_IMAGE, color = "black") +
                 geom_line(aes(y = fit), colour = "blue", size = 1)
 # print(summary(mpi$fit))
-# print(out_2)
+print(out_2)
 
 new.MAG_AUTO <- data.frame(wt=c(1.7, 2.4, 3.6))
 out_3 <- predict(modelo_poli5, new_data = SSOsDataFrame$MAG_AUTO, se.fit = TRUE, scale = NULL, df = Inf,
-                 interval = c("prediction"), level = 0.95, type = c("response"))
-# print(summary(out_3))
+                 interval = c("prediction"), level = prediction.level, type = c("response"))
+
 out_matrix <- out_3$fit
 fit_list <- out_matrix[, "fit"]
 lwr_list <- out_matrix[, "lwr"]
 upr_list <- out_matrix[, "upr"]
-print(max(fit_list))
+
+upr_limit = lm(upr_list ~ poly(SSOsDataFrame$MAG_AUTO, 4, raw=TRUE))
+# print(upr_limit$coefficients)
+lwr_limit = lm(lwr_list ~ poly(SSOsDataFrame$MAG_AUTO, 4, raw=TRUE))
+print(lwr_limit$coefficients)
+# plot(SSOsDataFrame$MAG_AUTO, lwr_list)
 
 
+plot(StarsDataFrame$MAG_AUTO, StarsDataFrame$B_IMAGE)
---
+++ regressions/sources.py
@@ -0,0 +1,50 @@
+from numpy import poly1d
+from pandas import read_csv
+
+# Reads SSOs catalogue
+ssos_df = read_csv('ssos_2.csv', index_col=0)
+
+print(ssos_df.columns)
+
+# Creates polynomical functions for limits
+upr_coefs_bright = [5.730178e-03, -6.528091e-01, 2.970536e+01,
+                  -6.748832e+02,  7.655361e+03, -3.468232e+04]
+upr_limit_bright = poly1d(upr_coefs_bright)
+lwr_coefs_bright = [5.735456e-03, -6.536302e-01, 2.975250e+01,
+                  -6.761721e+02, 7.672408e+03, -3.477074e+04]
+lwr_limit_bright = poly1d(lwr_coefs_bright)
+
+upr_coefs_faint = [-1.769922e-02, 1.871337e+00, -7.400582e+01,
+                  1.297378e+03, -8.505660e+03]
+upr_limit_faint = poly1d(upr_coefs_faint)
+lwr_coefs_faint = [-3.273145e-02, 3.398693e+00, -1.321923e+02,
+                  2.282417e+03, -1.475840e+04 ]
+lwr_limit_faint = poly1d(lwr_coefs_faint)
+
+in_bright = 0
+out_bright = 0
+in_faint = 0
+out_faint = 0
+# Iterate over sources
+for i, row in enumerate(ssos_df.itertuples(), 1):
+
+    if row.MAG_AUTO < 24.5:
+        b_low = lwr_limit_bright(float(row.MAG_AUTO))
+        b_upr = upr_limit_bright(float(row.MAG_AUTO))
+
+        if b_low < float(row.B_IMAGE) < b_upr:
+            in_bright += 1
+        else:
+            out_bright += 1
+    elif row.MAG_AUTO > 24.5:
+        b_low = lwr_limit_faint(float(row.MAG_AUTO))
+        b_upr = upr_limit_faint(float(row.MAG_AUTO))
+
+        if b_low < float(row.B_IMAGE) < b_upr:
+            in_faint += 1
+        else:
+	    out_faint += 1
+
+print('bright -- in {} - out {}'.format(in_bright, out_bright))
+print('faint  -- in {} - out {}'.format(in_faint, out_faint))
+
