b4bb15d68d8f5e11e4b5fc599541b081702b2bb4
Samuel Gongora-Garcia <s.gongoragarcia@gmail.com>
2018-09-10 11:16:06 +0200

    update

--- plot_movement.py
+++ plot_movement.py
@@ -18,10 +18,6 @@ from pandas import concat, read_csv, Series
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 
-from misc import extract_settings_elvis, check_source, setting_logger
-from misc import get_norm_mag, get_norm_speed
-from misc_cats import get_dither
-
 
 __author__ = "Samuel Góngora García"
 __copyright__ = "Copyright 2018"
@@ -37,7 +33,8 @@ class PlotFalseMovement:
         """
         """
         self.cats_d = {}
-        self.prfs_d = extract_settings_elvis()
+        # self.pms = [0.01, 0.03, 0.1, 0.3, 1.0, 3.0, 10.0, 30.0]
+        self.pms = [0.1]
         self.mags = ['20-21', '21-22', '22-23', '23-24', '24-25', '25-26']
 
         self.create_catalogue_dict()
@@ -48,16 +45,16 @@ class PlotFalseMovement:
 
         :return: catalogue
         """
-        for pm_ in self.prfs_d['pms']:
-            dir_ = 'false_positives/false_{}.csv'.format(pm_)
+        for pm_ in self.pms:
+            dir_ = 'false_positives/right_{}.csv'.format(pm_)
             self.cats_d[pm_] = read_csv(dir_, index_col=0)
 
     def plot_movement_to_pdf(self):
         """
         """
-        for pm_ in self.prfs_d['pms']:
+        for pm_ in self.pms:
             for mag_ in self.mags:
-                pdf_name = '{}_{}.pdf'.format(pm_, mag_)
+                pdf_name = '{}_{}_{}.pdf'.format('right', pm_, mag_)
                 # Creates pdf
                 with PdfPages(pdf_name) as pdf:
                     cat = self.cats_d[pm_]
@@ -67,12 +64,125 @@ class PlotFalseMovement:
                     for source_ in unique_sources:
                         fig = plt.figure(figsize=(16.53, 11.69), dpi=100)
                         ax = fig.add_subplot(1, 1, 1)
+                        title_ = 'source: {}\npm: {} - mag: {}'.format(source_,
+                                                                       pm_,
+                                                                       mag_)
+                        ax.set_title(title_)
 
                         source_df = cat[cat['SOURCE'].isin([source_])]
-                        ax.plot(source_df['ALPHA_J2000'].tolist(),
-                                source_df['DELTA_J2000'].tolist(), 'bs')
+
+                        dithers = source_df['DITHER'].tolist()
+
+                        alpha_list = source_df['ALPHA_J2000'].tolist()
+                        alpha_list_seconds = []
+                        for alpha_ in alpha_list:
+                            alpha_list_seconds.append(alpha_ * 3600)
+                        # Both in seconds
+                        max_alpha = max(alpha_list_seconds)
+                        min_alpha = min(alpha_list_seconds)
+
+                        delta_list = source_df['DELTA_J2000'].tolist()
+                        delta_list_seconds = []
+                        for delta_ in delta_list:
+                            delta_list_seconds.append(delta_ * 3600)
+                        # Both in seconds
+                        max_delta = max(delta_list_seconds)
+                        min_delta = min(delta_list_seconds)
+
+                        # Ticks alpha creation
+                        x_ticks = {}
+                        x_ticks_label = {}
+                        # steps_alpha = int((max_alpha - min_alpha) / 0.01)
+                        x_ticks['major_t'] = []
+                        x_ticks_label['major_t'] = []
+                        x_ticks['major_t'].append(min_alpha - 0.01)
+                        x_first_label = min_alpha - 0.01 - int(min_alpha - 0.01)
+                        x_first_label = float("{0:.3f}".format(x_first_label))
+                        x_ticks_label['major_t'].append(x_first_label)
+                        x_ticks['major_t'].append(min_alpha)
+                        x_second_label = min_alpha - int(min_alpha)
+                        x_second_label = float("{0:.3f}".format(x_second_label))
+                        x_ticks_label['major_t'].append(x_second_label)
+
+                        idx_alpha = 1
+                        alpha_step = min_alpha
+                        while alpha_step < max_alpha:
+                            alpha_step = min_alpha + idx_alpha * 0.01
+                            x_ticks['major_t'].append(alpha_step)
+                            x_step_label = alpha_step - int(alpha_step)
+                            x_step_label = float("{0:.3f}".format(x_step_label))
+                            x_ticks_label['major_t'].append(x_step_label)
+                            idx_alpha += 1
+
+                        x_ticks['minor_t'] = []
+                        x_ticks['minor_t'].append(min_alpha - 0.005)
+                        x_ticks['minor_t'].append(min_alpha)
+
+                        idx_alpha = 1
+                        alpha_step = min_alpha
+                        while alpha_step < max_alpha:
+                            alpha_step = min_alpha + idx_alpha * 0.005
+                            x_ticks['minor_t'].append(alpha_step)
+                            idx_alpha += 1
+
+                        # Ticks delta creation
+                        y_ticks = {}
+                        y_ticks_label = {}
+                        # steps_alpha = int((max_alpha - min_alpha) / 0.01)
+                        y_ticks['major_t'] = []
+                        y_ticks_label['major_t'] = []
+                        y_ticks['major_t'].append(min_delta - 0.01)
+                        y_first_label = min_delta - 0.01 - int(min_delta - 0.01)
+                        y_first_label = float("{0:.3f}".format(y_first_label))
+                        y_ticks_label['major_t'].append(y_first_label)
+                        y_ticks['major_t'].append(min_delta)
+                        y_second_label = min_delta - int(min_delta)
+                        y_second_label = float("{0:.3f}".format(y_second_label))
+
+                        idx_delta = 1
+                        delta_step = min_delta
+                        while delta_step < max_delta:
+                            delta_step = min_delta + idx_delta * 0.01
+                            y_ticks['major_t'].append(delta_step)
+                            y_step_label = delta_step - int(delta_step)
+                            y_step_label = float("{0:.3f}".format(y_step_label))
+                            y_ticks_label['major_t'].append(y_step_label)
+                            idx_delta += 1
+
+                        y_ticks['minor_t'] = []
+                        y_ticks['minor_t'].append(min_delta - 0.005)
+                        y_ticks['minor_t'].append(min_delta)
+                        idx_delta = 1
+                        delta_step = min_delta
+                        while delta_step < max_delta:
+                            delta_step = min_delta + idx_delta * 0.005
+                            y_ticks['minor_t'].append(delta_step)
+                            idx_delta += 1
+
+                        colors = ['r', 'b', 'y', 'g']
+                        for idx in range(0, len(alpha_list_seconds), 1):
+                            ax.scatter(alpha_list_seconds[idx],
+                                       delta_list_seconds[idx],
+                                       c=colors[idx], label=dithers[idx], s=36)
+
+                        ax.set_xticks(x_ticks['major_t'], minor=False)
+                        ax.set_xticklabels(x_ticks_label['major_t'])
+                        ax.set_xticks(x_ticks['minor_t'], minor=True)
+
+                        ax.set_yticks(y_ticks['major_t'], minor=False)
+                        ax.set_yticklabels(x_ticks_label['major_t'])
+                        ax.set_yticks(y_ticks['minor_t'], minor=True)
+
+                        # Formats grids
+                        ax.grid(b=True, which='major',
+                                linestyle='-', linewidth=1)
+                        ax.grid(b=True, which='minor',
+                                linestyle='--', linewidth=1)
+
+                        ax.legend()
 
                         pdf.savefig()
+                        plt.close(fig)
 
     #     for source_ in list(set(df['SOURCE'].tolist())):
     #         source_df = df[df['SOURCE'].isin([source_])]
